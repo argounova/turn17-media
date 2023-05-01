5df107a7f8c139dc0040de54a0c1e6e7d48382b9
Argounova <putzstuck@gmail.com>
2023-05-01 08:00:33 -0400

    banner header component

--- components/BannerHeader.jsx
+++ components/BannerHeader.jsx
@@ -5,19 +5,47 @@ function BannerHeader() {
     return(
         <Box
         sx={{
-            mx: '5%'
+            mx: '5%',
+            position: 'relative',
+            top: '-150px',
+            zIndex: '5',
         }}
         >
+            <h4
+            style={{
+                color: '#fff',
+                textAlign: 'center',
+            }}
+            >
+                Design a website on your terms with a <span style={{  color: '#88deeb'}}>clear pricing</span> structure.
+            </h4>
+            <p
+            style={{
+                color: '#c6c6c6',
+                textAlign: 'center',
+                textDecoration: 'line-through',
+            }}
+            >
+                If you have to ask, you can&apos;t afford it
+            </p>
             <Typography
-            variant="h5"
             sx={{
                 color: '#fff',
                 textAlign: 'center',
-                position: 'relative',
-                top: '-130px'
+                display: {xs: 'none', sm: 'block'}
             }}
             >
-            Design a website on your terms with a clear pricing structure.
+                Understand what you&apos;re getting and how much it will cost from day one.
+            </Typography>
+            <Typography
+            component='h3'
+            sx={{
+                color: '#fff',
+                textAlign: 'center',
+                display: {xs: 'none', sm: 'block'}
+            }}
+            >
+                A secure, professional website doesn&apos;t need to be complicated.
             </Typography>
         </Box>
     )
