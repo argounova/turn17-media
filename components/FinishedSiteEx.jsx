6fdc52542ccc0effa03785e55cd9c565f3474640
Argounova <putzstuck@gmail.com>
2023-05-02 13:20:09 -0400

    framer motion initial testing

--- components/FinishedSiteEx.jsx
+++ components/FinishedSiteEx.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion"
 import Image from "next/image";
 import finishedSiteEx from "../public/images/finished-site-example.png"
 import styles from "@/styles/FinishedSiteEx.module.css"
@@ -5,27 +6,33 @@ import styles from "@/styles/FinishedSiteEx.module.css"
 export default function FinishedSiteEx() {
     return(
         <>
-            <div>
-                <h2
+            <motion.div
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 1.5 }}
+            >
+                <div>
+                    <h2
+                        className={styles.common}
+                        style={{
+                            borderTopLeftRadius: '10px',
+                            borderTopRightRadius: '10px',
+                            padding: '2% 0px',
+                        }}
+                    >
+                        Your site.  Easy peasy, lemon squeezy.
+                    </h2>
+                </div>
+                <Image 
                     className={styles.common}
+                    src={finishedSiteEx}
+                    alt='finished site example'
                     style={{
-                        borderTopLeftRadius: '10px',
-                        borderTopRightRadius: '10px',
-                        padding: '2% 0px',
+                        borderBottomLeftRadius: '10px',
+                        borderBottomRightRadius: '10px',
                     }}
-                >
-                    Your site.  Easy peasy, lemon squeezy.
-                </h2>
-            </div>
-            <Image 
-                className={styles.common}
-                src={finishedSiteEx}
-                alt='finished site example'
-                style={{
-                    borderBottomLeftRadius: '10px',
-                    borderBottomRightRadius: '10px',
-                }}
-            />
+                />
+            </motion.div>
         </>
     )
 }
