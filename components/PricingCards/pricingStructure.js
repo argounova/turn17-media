1f2f78bfb4e2a26b9cefff4d1af0b08a05799183
Argounova <putzstuck@gmail.com>
2023-05-21 16:38:54 -0400

    pricing update

--- components/PricingCards/pricingStructure.js
+++ components/PricingCards/pricingStructure.js
@@ -3,7 +3,7 @@ const pricingStructure = [
         tier: '1',
         title: '1-2 Pages',
         subtitle: 'Far from basic, this one to two page site is packed with features.',
-        price: '$550 USD',
+        price: '$ USD',
         etc: 'ETC: Three business days',
         btnText: 'Get Started',
         href: '/get-started/singlepage'
@@ -12,7 +12,7 @@ const pricingStructure = [
         tier: '2',
         title: '3-4 Pages',
         subtitle: 'More real estate for your content, or room to grow into.',
-        price: '$990 USD',
+        price: '$ USD',
         etc: 'ETC: Six business days',
         btnText: 'Get Started',
         href: '/get-started/multipage'
@@ -21,7 +21,7 @@ const pricingStructure = [
         tier: '3',
         title: 'Custom',
         subtitle: `Need more?  Let's chat.`,
-        price: '$TBD',
+        price: '$ USD',
         etc: 'ETC: TBD',
         btnText: 'Contact Us',
         href: '/contact'
