7c1e0a1658f2b36da56f48a8e2b2a86da5f21fe6
Argounova <putzstuck@gmail.com>
2023-05-30 18:15:19 -0400

    latest page content and database connection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import clientPromise from '../../lib/mongodb'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
--- src/pages/latest.js
+++ src/pages/latest.js
@@ -1,25 +1,32 @@
 import Head from 'next/head'
 import clientPromise from '../../lib/mongodb'
 import styles from '@/styles/singlepage.module.css'
+import Divider from '@mui/material/Divider'
+import Image from 'next/image'
+import Stack from '@mui/material/Stack'
 import TopNavigation from '../../components/TopNavigation'
 import BannerHeader from '../../components/BannerHeader'
 import Footer from '../../components/Footer/Footer'
+import postImage from '../../public/images/post1thumb.jpg'
 
 export async function getServerSideProps() {
   try {
-    await clientPromise
+    const client = await clientPromise
+    const db = client.db("turn17_media")
+    const latest = await db
+      .collection("latest")
+      .find({})
+      .limit(10)
+      .toArray()
     return {
-      props: { isConnected: true },
+      props: { latest: JSON.parse(JSON.stringify(latest)) },
     }
   } catch (e) {
     console.error(e)
-    return {
-      props: { isConnected: false },
-    }
   }
 }
 
-export default function Latest({isConnected}) {
+export default function Latest({ latest }) {
   return (
     <>
       <Head>
@@ -30,12 +37,34 @@ export default function Latest({isConnected}) {
       </Head>
       <TopNavigation />
       <BannerHeader 
-        tag1='Under Construction'
+        tag1='Latest happenings from Turn17'
         // subtag='Hidden fees'
-        subtext1='Please bear with us as our site is being built.'
-        subtext2='Contact info@turn17media.com with any questions.'
+        subtext1='Check out some of our recent clients, new sites and even our extracurricular activites.'
+        // subtext2='Contact info@turn17media.com with any questions.'
       />
       <main className={styles.main}>
+        {latest.map((post) => (
+          <div className={styles.postDiv}>
+            <Stack 
+              direction={{ xs: 'column', sm: 'row' }}
+              divider={<Divider orientation="vertical" flexItem />}
+              spacing={2}
+            >
+              <Image
+                src={postImage}
+                alt='image'
+                style={{ width: 'auto', height: '300px' }}
+              />
+              <Stack gap={1}>
+                <h1>{post.title}</h1>
+                <h3>{post.subtitle}</h3>
+                <p>{post.dateStart} to {post.dateEnd}</p>
+                <p>{post.para}</p>
+                <p>Post by: {post.author}</p>
+              </Stack>
+            </Stack>
+          </div>
+        ))}
       </main>
       <Footer />
     </>
