85176d76bc9a930be5443a90eae291e75433dba2
Argounova <putzstuck@gmail.com>
2023-06-05 17:52:45 -0400

    api post route

---
+++ src/pages/api/mediaClients.js
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import clientPromise from "../../../lib/mongodb"
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      const client = await clientPromise
+      const db = client.db("turn17_media")
+      const collection = await db.collection("media_clients")
+      const mediaClients = await collection.find({}).toArray()
+
+      res.status(200).json({ mediaClients })
+    } catch (e) {
+      console.error(e)
+    }
+  } else if (req.method === "POST") {
+    try {
+      const newMediaClient = req.body
+      const client = await clientPromise
+      const db = client.db("turn17_media")
+      const collection = await db.collection("media_clients")
+      const mediaClients = await collection.insertOne(newMediaClient)
+
+      res.status(200).json({ mediaClients })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
