81e4462a80b5bb0ffcb0e3111966dffa914dbffd
Argounova <putzstuck@gmail.com>
2023-06-21 11:44:12 -0400

    coding sucks

--- components/UserSelections.jsx
+++ components/UserSelections.jsx
@@ -1,37 +1,27 @@
 import { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
+import useSWR from 'swr'
+
  
 const UserSelections = () => {
     const { data: session } = useSession()
+    const [isReady, setIsReady] = useState(false)
     const userEmail = session?.user.email
-    console.log('the email of current user is: ' + userEmail)
-    const [data, setData] = useState(null)
-    const [isLoading, setLoading] = useState(false)
+    const fetcher = (...args) => fetch(...args).then(res => res.json())
+    const { data, error } = useSWR('/api/routes/userData', fetcher)
+    console.log(data)
+    const searchIndex = data.findIndex((user) => user.email === userEmail) 
+    console.log(searchIndex)
+    // setIsReady(true)
 
-    useEffect(() => {
-    setLoading(true)
-    fetch('/api/routes/userData')
-        .then((res) => res.json())
-        .then((data) => {
-        setData(data)
-        setLoading(false)
-        })
-    }, [])
-
-        const userData = JSON.parse(JSON.stringify(data.users))
-        const searchIndex = userData.findIndex((user) => user.email === userEmail)
-        console.log(userData[searchIndex])
-
-    
-
-    // console.log('The array of user data is: ' + userData[0].email)
-    if (isLoading) return <p>Loading...</p>
-    if (!data) return <p>No profile data</p>
+    if (!isReady) return <p>Loading...</p>
+    if (error) return <p>No profile data</p>
 
     return (
     <div>
-        <h1>My name is {userData[searchIndex].name}</h1>
-        <p>My template choice is: {userData[searchIndex].template}</p>
+        <h1>My name is {data.name}</h1>
+        {/* <p>My template choice is: {userData[searchIndex].template}</p> */}
+        {/* <p>Color bypass?: {userData[searchIndex].colorBypass}</p> */}
     </div>
     )
     }
--- src/pages/api/routes/userData.js
+++ src/pages/api/routes/userData.js
@@ -1,13 +1,12 @@
 import clientPromise from "../../../../lib/mongodb"
 
 export default async function getProfile(req, res) {
-   if (req.method === "GET") {
+  if (req.method === "GET") {
     try {
       const client = await clientPromise
       const db = client.db("turn17_media")
-      const collection = await db.collection("users")
-      const users = await collection.find().toArray()
-      res.status(200).json({ users })
+      const collection = await db.collection("users").find({}).toArray()
+      res.status(200).json(collection)
     } catch (e) {
       console.error(e)
     }
