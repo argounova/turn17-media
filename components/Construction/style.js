ea743f02505135226b4b95ce48ef5793a3c88ec5
Craig Putzstuck <putzstuck@gmail.com>
2024-06-10 15:51:46 -0400

    under construction component added

---
+++ components/Construction/style.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled"
+
+export const ConstructionStyles = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+width: 100vw;
+background: rgb(0,0,0);
+background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(13,13,13,1) 85%, rgba(58,134,148,1) 100%, rgba(0,0,0,1) 100%);
+
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,25 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import Head from 'next/head'
-import TopNavigation from '../../components/TopNavigation'
-import BannerHeader from '../../components/BannerHeader'
-import MainFeatures from '../../components/MainFeatures'
-import DownArrow from '../../components/DownArrow'
-import BannerAnimA from '../../components/Banners/BannerAnimA'
-import BannerAnimB from '../../components/Banners/BannerAnimB'
-import BannerA from '../../components/Banners/BannerA'
-import BannerC from '../../components/Banners/BannerC'
-import BannerD from '../../components/Banners/BannerD'
-import FinishedSiteEx from '../../components/FinishedSiteExample'
-import Contact from '../../components/Contact'
-import Footer from '../../components/Footer/Footer'
-import ControlledModal from '../../components/ControlledModal'
-import CalendlyScheduler from '../../components/CalendlyScheduler/calendlyScheduler'
+// import TopNavigation from '../../components/TopNavigation'
+// import BannerHeader from '../../components/BannerHeader'
+// import MainFeatures from '../../components/MainFeatures'
+// import DownArrow from '../../components/DownArrow'
+// import BannerAnimA from '../../components/Banners/BannerAnimA'
+// import BannerAnimB from '../../components/Banners/BannerAnimB'
+// import BannerA from '../../components/Banners/BannerA'
+// import BannerC from '../../components/Banners/BannerC'
+// import BannerD from '../../components/Banners/BannerD'
+// import FinishedSiteEx from '../../components/FinishedSiteExample'
+// import Contact from '../../components/Contact'
+// import Footer from '../../components/Footer/Footer'
+// import ControlledModal from '../../components/ControlledModal'
+// import CalendlyScheduler from '../../components/CalendlyScheduler/calendlyScheduler'
+
+import Construction from '../../components/Construction'
 
 
 export default function Home() {
-  const [shouldShowModal, setShouldShowModal] = useState(false)
+  // const [shouldShowModal, setShouldShowModal] = useState(false)
   return (
     <>
       <Head>
@@ -26,16 +28,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/turn17-media-tab-logo.jpg" />
       </Head>
-      <TopNavigation />
-      <BannerHeader 
+      {/* <TopNavigation /> */}
+      {/* <BannerHeader 
         tag1='Welcome to Turn17 Media.'
         tag2='Discover affordable web design and photography solutions.'
         subtag='If you have to ask, you can&apos;t afford it'
         // subtext1='Understand what you&apos;re getting and how much it will cost from the beginning.'
         // subtext2='A secure, professional website doesn&apos;t need to be complicated.'
-      />
+      /> */}
       <main>
-        <MainFeatures 
+      <Construction />
+        {/* <MainFeatures 
           onClick={() => setShouldShowModal(!shouldShowModal)}
         />
         <BannerAnimA />
@@ -45,7 +48,7 @@ export default function Home() {
         <BannerD />
         <DownArrow />
         <FinishedSiteEx />
-        <Contact />
+        // <Contact />
         <div id='free-consultation' style={{ position: 'absolute', bottom: '800px' }}/>
         <ControlledModal
           shouldShow={shouldShowModal}
@@ -54,9 +57,9 @@ export default function Home() {
           }}
         >
           <CalendlyScheduler />
-        </ControlledModal>
+        </ControlledModal> */}
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
